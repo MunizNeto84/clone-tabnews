b640d12b9beb3a2eac875714a71f9d5bdfffc0eb
Muniz Neto <muniznetto@outlook.com>
2023-08-15 23:21:42 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Paola, meu amor. Eu te amo</h1>
+    return <h1>Paola, meu amor. Obrigado, por sempre estar comigo minha melhor companhia Eu te amo!</h1>
 }
 
 export default Home
