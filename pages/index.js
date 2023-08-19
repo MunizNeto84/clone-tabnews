ef69d8c41399e21eecf180647e34bd9ada58651e
Muniz Neto <muniznetto@outlook.com>
2023-08-19 15:40:30 -0300

    adiciona scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home () {
-    return <h1>Paola, meu amor. Obrigado, por sempre estar comigo minha melhor companhia Eu te amo!</h1>
+function Home() {
+  return (
+    <h1>
+      Paola, meu amor. Obrigado, por sempre estar comigo minha melhor companhia
+      Eu te amo!
+    </h1>
+  );
 }
 
-export default Home
+export default Home;
