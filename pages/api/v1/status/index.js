344bc68ebb27f2c46d385f2e929b9edac101b731
MunizNeto84 <muniznetto@outlook.com>
2023-12-17 23:30:22 -0300

    add `database.js` e `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(req, resp) {
+import database from "../../../../infra/database.js";
+
+async function status(req, resp) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   resp.status(200).json({
     chave: "Estou enviando uma resposta via http",
   });
