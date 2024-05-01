628771961064c4e0b8cdecdfee13d81eb50a8743
MunizNeto84 <muniznetto@outlook.com>
2024-05-01 20:00:50 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,33 @@
 import database from "infra/database.js";
 
 async function status(req, resp) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseMaxConectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsValue = parseInt(
+    databaseMaxConectionsResult.rows[0].max_connections,
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
   resp.status(200).json({
-    chave: "Estou enviando uma resposta via http",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        opened_connections: databaseOpenedConnectionsValue,
+        max_connections: databaseMaxConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
